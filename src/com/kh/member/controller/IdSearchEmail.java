fe49cb2fd8196b72884a443b5cf84551a98f03e6
dheenk <dheenk@DESKTOP-K1RL6SA>
2020-03-30 12:30:11 +0900

    메일 api 수정

--- src/com/kh/member/controller/IdSearchEmail.java
+++ src/com/kh/member/controller/IdSearchEmail.java
@@ -1,14 +1,23 @@
 package com.kh.member.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.kh.member.model.service.MemberService;
-import com.kh.member.model.vo.Member;
+import com.kh.mail.MailSender;
 
 /**
  * Servlet implementation class IdSearchEmail
@@ -29,9 +38,77 @@ public class IdSearchEmail extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * String idSearch = request.getParameter("idSearch");
+		 * request.setAttribute("idSearch", idSearch);
+		 * request.getRequestDispatcher("views/member/sendPwdMail.jsp").forward(request,
+		 * response);
+		 */
+		
+		String from = "bookeonandon@gmail.com";
 		String idSearch = request.getParameter("idSearch");
-		request.setAttribute("idSearch", idSearch);
-		request.getRequestDispatcher("views/member/sendPwdMail.jsp").forward(request, response);
+		String idSearchId = request.getParameter("idSearchId");
+		String subject = "[BOOK E ON&ON] 임시비밀번호 입니다.";
+		String content = "안녕하세요 BOOK E ON&ON 입니다. <br>";
+		
+		char[] charSet = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' }; 
+		int idx = 0; 
+		StringBuffer sb = new StringBuffer();
+		for (int i=0; i<10; i++) { 
+			idx = (int)(charSet.length * Math.random()); 
+			sb.append(charSet[idx]);
+			} 
+		String ranNum = sb.toString();
+		
+		Properties p = new Properties(); // 정보를 담을 객체
+		 
+		p.put("mail.smtp.host","smtp.gmail.com"); // 구글 SMTP
+		p.put("mail.smtp.port", "465");
+		p.put("mail.smtp.starttls.enable", "true");
+		p.put("mail.smtp.auth", "true");
+		p.put("mail.smtp.debug", "true");
+		p.put("mail.smtp.socketFactory.port", "465");
+		p.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+		p.put("mail.smtp.socketFactory.fallback", "false");
+		
+		try{
+		    Authenticator auth = new MailSender();
+		    Session ses = Session.getInstance(p, auth);
+		     
+		    ses.setDebug(true);
+		     
+		    MimeMessage msg = new MimeMessage(ses); // 메일의 내용을 담을 객체
+  
+		    msg.setSubject(subject); // 제목
+		     
+		    Address fromAddr = new InternetAddress(from);
+		    msg.setFrom(fromAddr); // 보내는 사람
+		     
+		    Address toAddr = new InternetAddress(idSearch);
+		    msg.addRecipient(Message.RecipientType.TO, toAddr); // 받는 사람
+		     
+		    msg.setContent(content +  "전체 아이디 : "  + "<b>" + idSearchId + "</b> <br>" + "임시 비밀번호 : " + "<b>" + ranNum +"</b>"+ "<br>임시 비밀번호 입력후 비밀번호 재설정 해주세요 ","text/html;charset=UTF-8"); // 내용과 인코딩
+		     
+		    Transport.send(msg); // 전송
+		    
+		} catch(Exception e){
+		    e.printStackTrace();
+		    
+		    response.getWriter().println("<script>alert('Email 발송에 실패했습니다.');history.back();</script>");
+		    // 오류 발생시 뒤로 돌아가도록
+		    return;
+		}
+		//out.println("<script>alert('메일 발송이 완료됐습니다.'); $('#ranNumPwd').submit(); </script>");		
+		
+		//response.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("<form action='updatePwdForm.me' method='post' id='pwdForm'>");
+		out.println("<input type='hidden' name='ranNum' value='" + ranNum + "'>");
+		out.println("</form>");
+		out.println("<script>alert('메일 발송이 완료됐습니다.'); document.getElementById('pwdForm').submit();</script>");		
 	}
 
 	/**
--- src/com/kh/member/controller/updatePwd.jsp
+++ src/com/kh/member/controller/updatePwd.jsp
@@ -1,12 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    
-<%@ page import="com.kh.member.model.vo.Member"%>
-<%
-	Member mem = (Member)request.getAttribute("mem");
-	String result = (String)request.getAttribute("result");
-%>    
-
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,7 +8,7 @@
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
 	<link href="<%=request.getContextPath() %>/resources/css/style.css" rel="stylesheet">
-	<title>memberSearchId</title>
+	<title>updatePwd</title>
 	<script src="<%=request.getContextPath() %>/resources/plugins/common/common.min.js"></script>
     <script src="<%=request.getContextPath() %>/resources/js/custom.min.js"></script>
     <script src="<%=request.getContextPath() %>/resources/js/settings.js"></script>
@@ -32,25 +25,24 @@
                         <div class="card login-form mb-0">
                             <div class="card-body pt-5">
                                 <a class="text-center" href=""> <h4>BOOK E ON & ON</h4></a>
-                                <form class="mt-5 mb-3 login-input" method="post" action="idSearchEmail.me">
+                                <form class="mt-5 mb-3 login-input">
                                     <div class="form-group">
                                         <div>
                 
-                                            <blockquote class="text-center">개인정보 보호를 위해 뒤 4글자는 보이지 않습니다.<br>
-                                                <span> <%=result %> </span>
+                                            <blockquote class="text-center">
+                                                <p></p>
+                                                <span>비밀번호 재설정 완료!</span>
+                                                <p></p>
                                             </blockquote>
-                                            <input type="hidden" name="idSearch" value="<%= mem.getEmail()%>">
-                                            <input type="hidden" name="idSearchId" value="<%= mem.getMemberId()%>">
+                                            
+                                            
                                         </div>
                                     </div>
                                     <div>
-                                        <h6 class="card-subtitle text-center">전체 아이디 + 임시 비밀번호</h6>
-                                        <button type="submit" class="btn login-form__btn submit w-100">Email 발송</button>
-                                    </div>
-                                    <div id="idsearch">
-                                        <a onclick="loginForm();" style="cursor:pointer;" class="text-primary">로그인</a>
-                                    </div>
                                         
+                                        <button type="button" class="btn login-form__btn submit w-100" onclick="loginForm();">로그인</button>
+                                    </div>
+                       
                                 </form>
                                 </div>
                             </div>
@@ -59,23 +51,14 @@
                 </div>
             </div>
         </div>
+		<script>
+			function loginForm(){
+				location.href = "<%=request.getContextPath()%>/loginPath.me";
+	    	}
+		
+		</script>
 
-	<script>
-	
-		
-		
-		function idSearchEmail(){
-			location.href= "<%=request.getContextPath()%>/idSearchEmail.me";
-		}
-		
-	
-		function loginForm(){
-			location.href = "<%=request.getContextPath()%>/loginPath.me";
-		}
-		
-		
-	</script>
-	
     
+
 </body>
 </html>
